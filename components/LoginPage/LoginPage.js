f166ea0e64aec7d728fa215a6268d5b41cf052d7
prathameshpatil5181 <80106674+prathameshpatil5181@users.noreply.github.com>
2023-11-05 22:54:01 +0530

    Revert "Main"

--- components/LoginPage/LoginPage.js
+++ components/LoginPage/LoginPage.js
@@ -1,37 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./LoginPage.module.css";
 import Link from "next/link";
+
+const emailValid = (email)=>{
+  return email.includes('@');
+}
+
 const LoginPage = () => {
+
+  const [email,setEmail] = useState('');
+  const [isEmailValid,setIsEmailValid]=useState(true);
+  const [isEmailTouched,setIsEmailTouched] = useState(false);
+
+  const emailChangeHandler = (event)=>{
+    setIsEmailTouched(true);
+    setEmail(event.target.value);
+    setIsEmailValid(emailValid(event.target.value));
+    console.log(event.target.value);
+  }
+
   return (
     <div className={classes.main}>
       <div className={classes.form}>
-        <div style={{ display:"flex", flexDirection:'column',
-       justifyContent:'center',
-       alignItems:'left'
-    
-    }}>
-        <form className={classes.credentials}>
-          <ul>
-            <li>
-              <input type="text" placeholder="Username" />
-              {/* <span>eye</span> */}
-            </li>
-            <li>
-              <input type="text" placeholder="Password" />
-              {/* <span>eye</span> */}
-            </li>
-            <button>Login</button>
-          </ul>
-        </form>
-        <div className={classes.options}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "left",
+          }}
+        >
+          <form className={classes.credentials}>
             <ul>
-                <li>Forgot Password</li>
-                <Link href='/Signup'><li>Sign Up</li></Link>
+              <li>
+                <input type="text" placeholder="Username" onChange={emailChangeHandler} value={email}/>
+                
+              </li>
+              <li>{isEmailTouched&&isEmailValid?<p>Please enter valid email</p>:''}</li>
+              <li>
+                <input type="text" placeholder="Password" />
+              </li>
+              <button>Login</button>
             </ul>
-        </div>
+          </form>
+          <div className={classes.options}>
+            <ul>
+              <li>Forgot Password</li>
+              <Link href="/Signup">
+                <li>Sign Up</li>
+              </Link>
+            </ul>
+          </div>
         </div>
         <div className={classes.line}></div>
-      <div className={classes.text}>Welcome Back Login to Continue</div>
+        <div className={classes.text}><p>Welcome Back</p><p>Login to Continue</p> </div>
       </div>
     </div>
   );
